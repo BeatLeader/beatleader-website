f2210311ebcffca6f6d1f38b388780a0e6e84e20
motzel <motzel@o2.pl>
2021-06-26 19:45:01 +0200

    feat: shorten the player profile refresh interval to 5 seconds for the very first fetch after entering/refreshing the page

--- src/stores/http/providers/api-player-with-scores.js
+++ src/stores/http/providers/api-player-with-scores.js
@@ -1,7 +1,7 @@
 import createPlayerService from '../../../services/scoresaber/player';
 import createScoresService from '../../../services/scoresaber/scores';
 import queue from '../../../network/queues'
-import {MINUTE} from '../../../utils/date'
+import {MINUTE, SECOND} from '../../../utils/date'
 
 let playerService = null;
 let scoresService = null;
@@ -10,16 +10,19 @@ export default () => {
   playerService = createPlayerService();
   scoresService = createScoresService();
 
+  let firstFetch = true;
+
   return {
     getProcessed: async ({playerId, priority = queue.PRIORITY.FG_HIGH, scoresType = 'recent', scoresPage = 1, signal = null} = {}) => {
-      // TODO: find a way to force refreshing when user press reload button
-
       // TODO: force refresh scores for currently displayed profile (add a add/removeVisiblePlayer() method to download-manager maybe?)
-
       // TODO: and then increase refresh interval if player has not played recently (last 30 mins?)
-      const player = await playerService.fetchPlayerOrGetFromCache(playerId, MINUTE, priority, signal);
 
-      const scores = await scoresService.fetchScoresPageOrGetFromCache(player, scoresType, scoresPage, MINUTE, priority, signal)
+      const refreshInterval = firstFetch ? 5 * SECOND : MINUTE;
+      firstFetch = false;
+
+      const player = await playerService.fetchPlayerOrGetFromCache(playerId, refreshInterval, priority, signal);
+
+      const scores = await scoresService.fetchScoresPageOrGetFromCache(player, scoresType, scoresPage, refreshInterval, priority, signal)
 
       return {...player, scores, scoresType, scoresPage}
     },
