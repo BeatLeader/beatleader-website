99b645979474a5a39e33a89aec492546e9c5d4ed
motzel <motzel@o2.pl>
2021-07-17 11:06:35 +0200

    fix: friends menu is closed after selecting player

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -6,6 +6,7 @@
   import QueueStats from './Common/QueueStats.svelte'
 
   function onFriendClick(event) {
+    friendsMenuShown = false;
     navigate(`/u/${event.detail.playerId}/recent/1`)
   }
 
@@ -20,7 +21,7 @@
     Home
   </div>
 
-  <div class="friends" on:mouseenter={() => friendsMenuShown = true} on:mouseleave={() => friendsMenuShown = false}>
+  <div class="friends" on:mouseover={() => friendsMenuShown = true} on:mouseleave={() => friendsMenuShown = false}>
     <svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"></path></svg>
     Friends
 
