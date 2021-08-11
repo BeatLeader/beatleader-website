1b84e84972f5f076499d7a4800cbebd0300f6306
motzel <motzel@o2.pl>
2021-08-11 15:05:26 +0200

    fix: Beat Saver processor takes last version now instead of first

--- src/network/clients/beatmaps/utils/process.js
+++ src/network/clients/beatmaps/utils/process.js
@@ -1,7 +1,12 @@
 import {opt} from '../../../../utils/js'
 
 export default response => {
-  const hash = opt(response, 'versions.0.hash');
+  const versions = opt(response, 'versions');
+  if (!versions || !Array.isArray(versions) || !versions.length) return null;
+
+  const lastIdx = versions.length - 1;
+
+  const hash = opt(versions, `${lastIdx}.hash`);
   const key = opt(response, 'id');
 
   if (!hash || !key || !hash.toLowerCase) return null;
