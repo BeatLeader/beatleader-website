30bccef79cc72944fed068c233b7afb2f0bf2c57
motzel <motzel@o2.pl>
2021-09-10 16:24:02 +0200

    feat: add sweepable player profile cards

--- src/components/Player/Profile.svelte
+++ src/components/Player/Profile.svelte
@@ -1,7 +1,9 @@
 <script>
+  import {onMount, tick} from 'svelte'
   import processPlayerData from './utils/profile';
   import eventBus from '../../utils/broadcast-channel-pubsub'
   import {worker} from '../../utils/worker-wrappers'
+  import { Swipe, SwipeItem } from "svelte-swipe";
   import Error from '../Common/Error.svelte'
   import Avatar from './Avatar.svelte'
   import PlayerStats from './PlayerStats.svelte'
@@ -18,6 +20,21 @@
   eventBus.on('player-stats-calculated', stats => playerStats = stats)
 
   let onePpBoundery = null;
+
+  let swipeComponent = null;
+  let swipeHolderHeight = 0;
+  let activeSwipeItem = null;
+  const swipeConfig = {
+    autoplay: false,
+    delay: 5000,
+    showIndicators: true,
+    transitionDuration: 300,
+    defaultIndex: 0,
+  };
+
+  function onHeightChanged({detail}) {
+    swipeHolderHeight = detail.height + 32;
+  }
 
   function clearPlayerStatsOnChange() {
     playerStats = null;
@@ -49,13 +66,37 @@
       )
   }
 
+  let shouldRefreshHeight = true;
+  async function forceRefreshHeight(noResizeEvent = false) {
+    if (swipeComponent) swipeComponent.goTo(0);
+
+    shouldRefreshHeight = false;
+    await tick();
+    shouldRefreshHeight = true;
+
+    // swipe component bug workaround on small screens
+    if (!noResizeEvent) window.dispatchEvent(new Event('resize'));
+  }
+
+  onMount(() => {
+    const callForceRefreshHeight = () => forceRefreshHeight(true);
+
+    window.addEventListener('resize', callForceRefreshHeight);
+
+    return () => {
+      window.removeEventListener('resize', callForceRefreshHeight)
+    }
+  })
+
   $: isCached = !!(playerData && playerData.scoresLastUpdated)
   $: clearPlayerStatsOnChange(playerId)
   $: playerId = playerData && playerData.playerId ? playerData.playerId : null;
   $: name = playerData && playerData.name ? playerData.name : null;
   $: ({playerInfo, prevInfo, scoresStats, accStats, accBadges, ssBadges} = processPlayerData(playerData, playerStats))
+  $: (scoresStats, accStats, accBadges, ssBadges, forceRefreshHeight(), setTimeout(() => forceRefreshHeight(), 600))
   $: calcOnePpBoundary(playerId);
   $: scoresStatsFinal = generateScoresStats(scoresStats, onePpBoundery)
+
 </script>
 
 <div class="box has-shadow" class:loading={isLoading}>
@@ -69,24 +110,62 @@
     </div>
 
     <div class="column">
-      {#if error}
-        <div>
-          <Error {error}/>
-        </div>
-      {/if}
-
       <PlayerStats {name} {playerInfo} {prevInfo} {skeleton}/>
 
-      {#if scoresStats || ssBadges || skeleton}
-        <div class="stats" class:enhanced={isCached}>
-          <ScoresStats stats={scoresStatsFinal} {skeleton}/>
-          <div>
-            {#if accStats}<ScoresStats stats={accStats}/>{/if}
-            {#if accBadges}<ScoresStats stats={accBadges}/>{/if}
-          </div>
-          <SsBadges badges={ssBadges}/>
-        </div>
-      {/if}
+      <div class="swipe-container"  style="height:{swipeHolderHeight}px">
+        <Swipe bind:this={swipeComponent} bind:active_item={activeSwipeItem} {...swipeConfig}>
+          <SwipeItem
+            active={activeSwipeItem === 0 && shouldRefreshHeight}
+            allow_dynamic_height={true}
+            on:swipe_item_height_change={onHeightChanged}>
+            <div>
+              {#if error}
+                <div>
+                  <Error {error}/>
+                </div>
+              {/if}
+
+              {#if scoresStats || ssBadges || skeleton}
+                <div class="stats" class:enhanced={isCached}>
+                  <ScoresStats stats={scoresStatsFinal} {skeleton}/>
+                  <div>
+                    {#if accStats}<ScoresStats stats={accStats}/>{/if}
+                    {#if accBadges}<ScoresStats stats={accBadges}/>{/if}
+                  </div>
+                  <SsBadges badges={ssBadges}/>
+                </div>
+              {/if}
+            </div>
+          </SwipeItem>
+
+          <SwipeItem
+            active={activeSwipeItem === 1}
+            allow_dynamic_height={true}
+            on:swipe_item_height_change={onHeightChanged}>
+            <div>
+              SECOND
+            </div>
+          </SwipeItem>
+
+          <SwipeItem
+            active={activeSwipeItem === 2}
+            allow_dynamic_height={true}
+            on:swipe_item_height_change={onHeightChanged}>
+            <div>
+              THIRD
+            </div>
+          </SwipeItem>
+
+          <SwipeItem
+            active={activeSwipeItem === 3}
+            allow_dynamic_height={true}
+            on:swipe_item_height_change={onHeightChanged}>
+            <div>
+              FOURTH
+            </div>
+          </SwipeItem>
+        </Swipe>
+      </div>
     </div>
   </div>
 </div>
@@ -98,6 +177,10 @@
         margin-right: 1rem;
         min-width: 150px;
         min-height: 190px;
+    }
+
+    .swipe-container {
+        min-height: 120px;
     }
 
     @media screen and (min-width: 1200px) {
