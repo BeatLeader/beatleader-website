a675ed50edfa852a99776f64811971bce0532c8f
motzel <motzel@o2.pl>
2021-10-20 18:28:35 +0200

    fix: acc for old ranked maps

--- src/stores/http/enhancers/common/acc-calc.js
+++ src/stores/http/enhancers/common/acc-calc.js
@@ -1,35 +1,30 @@
-import {opt} from '../../../../utils/js'
-import {
-  getFixedLeaderboardMaxScore, getMaxScore,
-} from '../../../../utils/scoresaber/song'
+import {getFixedLeaderboardMaxScore, getMaxScore} from '../../../../utils/scoresaber/song'
 
 export default (score, bmStats, leaderboardId) => {
   let maxScore;
 
   if (bmStats && bmStats.notes) {
     maxScore = getMaxScore(bmStats.notes)
-  } else if(leaderboardId) {
+  } else if (leaderboardId) {
     maxScore = getFixedLeaderboardMaxScore(leaderboardId, score?.maxScore ?? null)
   }
+
   if (maxScore) {
     score.maxScore = maxScore;
   }
 
-  if (!score.acc && score.maxScore) {
-    let unmodifiedScore = opt(score, 'unmodifiedScore', opt(score, 'score'));
-    if (!unmodifiedScore) unmodifiedScore = opt(score, 'score', null);
+  let unmodifiedScore = score?.unmodifiedScore ?? score?.score ?? null;
+  if (!unmodifiedScore) unmodifiedScore = score?.score ?? null;
 
-    if (unmodifiedScore) {
-      score.maxScore = maxScore;
-      score.acc = unmodifiedScore ? unmodifiedScore / maxScore * 100 : null;
+  if (unmodifiedScore && score.maxScore) {
+    score.acc = unmodifiedScore ? unmodifiedScore / maxScore * 100 : null;
 
-      if (score.score) score.percentage = score.score / score.maxScore * 100;
-    }
+    if (score.score) score.percentage = score.score / score.maxScore * 100;
   }
 
-  if (!score.percentage && score.score && score.maxScore) {
+  if (score?.score && score?.maxScore) {
     score.percentage = score.score / score.maxScore * 100;
   }
-  
+
   return score;
 }
