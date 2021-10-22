0b6b8f5dec4c4878a16152dfd001cf371ece279d
motzel <motzel@o2.pl>
2021-10-22 10:54:19 +0200

    fix: player daily gains are accurate now

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,19 +1,16 @@
-import {addToDate, DAY, truncateDate} from '../utils/date'
+import {DateTime} from 'luxon';
 
 export const getServicePlayerGain = (playerHistory, dateTruncFunc, dateKey, daysAgo = 1, maxDaysAgo = 7) => {
   if (!playerHistory?.length) return null;
 
   let todayServiceMidnightDate = dateTruncFunc(new Date());
 
-  // fix the compared date if service day is different from the local one
-  const serviceVsLocalDiff = truncateDate(todayServiceMidnightDate).getTime() - truncateDate(new Date()).getTime();
-  todayServiceMidnightDate = addToDate(-serviceVsLocalDiff, todayServiceMidnightDate);
+  const compareToDate = DateTime.fromJSDate(todayServiceMidnightDate).minus({days: daysAgo}).toJSDate();
+  const maxServiceDate = DateTime.fromJSDate(todayServiceMidnightDate).minus({days: maxDaysAgo}).toJSDate();
 
-  const compareAccSaberDate = todayServiceMidnightDate  - DAY * daysAgo;
-  const maxServiceDate = todayServiceMidnightDate - DAY * maxDaysAgo;
   return playerHistory
     .sort((a, b) => b?.[dateKey]?.getTime() - a?.[dateKey]?.getTime())
-    .find(h => h?.[dateKey] <= compareAccSaberDate && h?.[dateKey] >= maxServiceDate);
+    .find(h => h?.[dateKey] <= compareToDate && h?.[dateKey] >= maxServiceDate);
 }
 
 export const serviceFilterFunc = serviceParams => s => {
