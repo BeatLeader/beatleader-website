e50cf6ba1e1dde781a6c56e87ad7883e1ae83d82
motzel <motzel@o2.pl>
2022-03-24 14:35:30 +0100

    fix: score statistics uncaught exception when 404

--- src/stores/http/enhancers/scores/scoreStatistic.js
+++ src/stores/http/enhancers/scores/scoreStatistic.js
@@ -4,31 +4,34 @@ import {BL_API_URL} from '../../../../network/queues/beatleader/api-queue'
 export default async (data) => {
     if (!data || data.beatSavior) return;
 
-    let response = await fetch(BL_API_URL + "score/statistic/" + data.score.id);
-    let statistic = await response.json();
-    let beatSavior = {};
+    try {
+        let response = await fetch(BL_API_URL + "score/statistic/" + data.score.id);
+        let statistic = await response.json();
+        let beatSavior = {};
 
-    var stats = statistic.accuracyTracker;
+        var stats = statistic.accuracyTracker;
 
-    stats.missedNotes = data.score.missedNotes;
-    stats.badCuts = data.score.badCuts;
-    stats.bombHit = data.score.bombCuts;
-    stats.wallHit = data.score.wallsHit;
-    stats.miss = stats.missedNotes + stats.badCuts + stats.bombHit + stats.wallHit;
-    stats.maxCombo = statistic.hitTracker.maxCombo;
-    stats.leftMiss = statistic.hitTracker.leftMiss;
-    stats.rightMiss = statistic.hitTracker.rightMiss;
-    stats.leftBadCuts = statistic.hitTracker.leftBadCuts;
-    stats.rightBadCuts = statistic.hitTracker.rightBadCuts;
-    stats.leftBombs = statistic.hitTracker.leftBombs;
-    stats.rightBombs = statistic.hitTracker.rightBombs;
+        stats.missedNotes = data.score.missedNotes;
+        stats.badCuts = data.score.badCuts;
+        stats.bombHit = data.score.bombCuts;
+        stats.wallHit = data.score.wallsHit;
+        stats.miss = stats.missedNotes + stats.badCuts + stats.bombHit + stats.wallHit;
+        stats.maxCombo = statistic.hitTracker.maxCombo;
+        stats.leftMiss = statistic.hitTracker.leftMiss;
+        stats.rightMiss = statistic.hitTracker.rightMiss;
+        stats.leftBadCuts = statistic.hitTracker.leftBadCuts;
+        stats.rightBadCuts = statistic.hitTracker.rightBadCuts;
+        stats.leftBombs = statistic.hitTracker.leftBombs;
+        stats.rightBombs = statistic.hitTracker.rightBombs;
 
-    stats.won = statistic.winTracker.won;
-    stats.pauses = statistic.winTracker.nbOfPause;
+        stats.won = statistic.winTracker.won;
+        stats.pauses = statistic.winTracker.nbOfPause;
 
-    beatSavior.songJumpDistance = statistic.winTracker.jumpDistance;
-    beatSavior.stats = stats;
-    beatSavior.trackers = statistic;
+        beatSavior.songJumpDistance = statistic.winTracker.jumpDistance;
+        beatSavior.stats = stats;
+        beatSavior.trackers = statistic;
 
-    data.beatSavior = beatSavior;
+        data.beatSavior = beatSavior;
+    }
+    catch(err) {}
 }
