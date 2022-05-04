098e0e57c94092d73f356d323074036421c66380
motzel <motzel@o2.pl>
2022-05-04 23:42:14 +0200

    feat: acc history chart now use data from the server

--- src/components/Player/Charts/AccHistoryChart.svelte
+++ src/components/Player/Charts/AccHistoryChart.svelte
@@ -6,14 +6,14 @@
     formatDateWithOptions,
     toBlMidnight,
   } from '../../../utils/date'
-  import createPlayerService from '../../../services/beatleader/player'
   import {debounce} from '../../../utils/debounce'
   import {onLegendClick} from './utils/legend-click-handler'
   import {getContext} from 'svelte'
   import {DateTime} from 'luxon'
+  import stringify from 'json-stable-stringify'
 
   export let playerId = null;
-  export let rankHistory = null;
+  export let statsHistory = null;
   export let height = "350px";
 
   const CHART_DAYS = 50;
@@ -21,45 +21,32 @@
 
   const pageContainer = getContext('pageContainer');
 
-  const playerService = createPlayerService();
-
   let canvas = null;
   let chart = null;
 
   let lastHistoryHash = null;
-  let playerHistory = null;
 
-  const calcHistoryHash = (accHistory) =>
-    (accHistory ? Object.values(accHistory).map(h => Object.values(h).join(',')).join(':') : '')
-  ;
-
-  async function refreshPlayerHistory(playerId) {
-    if (!playerId) return;
-
-    playerHistory = await playerService.getPlayerHistory(playerId) ?? null;
-  }
+  const calcHistoryHash = (accHistory) => stringify(accHistory);
 
   async function setupChart(hash, canvas) {
-    if (!hash || !canvas || chartHash === lastHistoryHash) return;
+    if (!hash || !canvas || chartHash === lastHistoryHash || !statsHistory?.averageAccuracy?.length) return;
 
     lastHistoryHash = chartHash;
 
-    if (rankHistory.length < CHART_DAYS) rankHistory = Array(CHART_DAYS - rankHistory.length).fill(null).concat(rankHistory);
+    const firstNonNullIndex = statsHistory.averageAccuracy.findIndex(a => a !== null);
+    const accHistory = statsHistory.averageAccuracy.slice(firstNonNullIndex);
+    const CHART_DAYS = accHistory.length;
 
     const gridColor = '#2a2a2a'
     const averageColor = '#3273dc';
+    const averageRankedColor = '#3e95cd';
     const medianColor = '#8992e8';
-    const stdDevColor = '#f94022';
-    const ssPlusColor = 'rgba(143,72,219, .4)';
-    const ssColor = 'rgba(190,42,66, .4)';
-    const sPlusColor = 'rgba(255,99,71, .4)';
-    const sColor = 'rgba(89,176,244, .4)';
-    const aColor = 'rgba(60,179,113, .4)';
+    const medianRankedColor = '#565b92';
 
     const datasets = [];
 
-    const dtAccSaberToday = DateTime.fromJSDate(toBlMidnight(new Date()));
-    const dayTimestamps = Array(CHART_DAYS).fill(0).map((_, idx) => toBlMidnight(dtAccSaberToday.minus({days: CHART_DAYS - 1 - idx}).toJSDate()).getTime());
+    const dtBlToday = DateTime.fromJSDate(toBlMidnight(new Date()));
+    const dayTimestamps = accHistory.map((_, idx) => toBlMidnight(dtBlToday.minus({days: CHART_DAYS - 1 - idx}).toJSDate()).getTime());
 
     const xAxis = {
       type: 'time',
@@ -98,8 +85,6 @@
       },
     };
 
-    const isScoreDataAvailable = playerHistory && playerHistory.find(h => !!h.avgAcc);
-
     let yAxes = {
       y: {
         display: true,
@@ -110,7 +95,7 @@
         },
         ticks: {
           callback: val => formatNumber(val, 2) + '%',
-          precision: 2
+          precision: 2,
         },
         grid: {
           color: gridColor,
@@ -118,184 +103,130 @@
       },
     };
 
-    if (additionalHistory && Object.keys(additionalHistory).length) {
-      if (isScoreDataAvailable)
-        yAxes = {
-          ...yAxes,
-          y1: {
-            display: true,
-            position: 'right',
-            title: {
-              display: $pageContainer.name !== 'phone',
-              text: 'Maps count',
-            },
-            ticks: {
-              callback: val => val === Math.floor(val) ? val : null,
-              precision: 0,
-            },
-            grid: {
-              drawOnChartArea: false,
-            },
+    const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
+
+    [
+      {
+        key: 'averageAccuracy',
+        name: 'Average',
+        borderColor: averageColor,
+        round: 0,
+        axisDisplay: false,
+        precision: 0,
+      },
+      {
+        key: 'averageRankedAccuracy',
+        name: 'Average ranked',
+        borderColor: averageRankedColor,
+        round: 0,
+        axisDisplay: false,
+        precision: 0,
+      },
+      {
+        key: 'medianAccuracy',
+        name: 'Median',
+        borderColor: medianColor,
+        round: 0,
+        axisDisplay: false,
+        precision: 0,
+      },
+      {
+        key: 'medianRankedAccuracy',
+        name: 'Median ranked',
+        borderColor: medianRankedColor,
+        round: 0,
+        axisDisplay: false,
+        precision: 0,
+      },
+    ]
+      .forEach(obj => {
+        const {key, name, axisDisplay, usePrevAxis, precision, reverse, ...options} = obj;
+
+        if (!statsHistory?.[key]) return;
+
+        const fieldData = dayTimestamps
+          .map((x, idx) => ({x, y: statsHistory?.[key]?.[idx + firstNonNullIndex] ?? null}))
+          .map(v => ({...v, y: v.y === 0 ? null : v.y}));
+
+        if (!fieldData.some(v => v.y)) return;
+
+        datasets.push({
+          ...options,
+          yAxisID: 'y',
+          label: name,
+          data: fieldData,
+          fill: false,
+          borderWidth: 2,
+          pointRadius: 1,
+          cubicInterpolationMode: 'monotone',
+          tension: 0.4,
+          type: 'line',
+          spanGaps: true,
+          segment: {
+            borderWidth: ctx => skipped(ctx, 1),
+            borderDash: ctx => skipped(ctx, [6, 6]),
           },
-
-          y2: {
-            display: false,
-            position: 'right',
-            title: {
-              display: $pageContainer.name !== 'phone',
-              text: 'Std dev',
-            },
-            ticks: {
-              callback: val => val,
-              precision: 3,
-            },
-            grid: {
-              color: gridColor,
-            },
-          },
-        }
-
-      const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
-
-      [
-        {
-          key: 'avgAcc',
-          secKey: 'averageRankedAccuracy',
-          name: 'Average',
-          borderColor: averageColor,
-          round: 2,
-          axis: 'y',
-          axisOrder: 0,
-        },
-      ]
-        .concat(
-          isScoreDataAvailable
-            ? [
-              {key: 'medianAcc', name: 'Median', borderColor: medianColor, round: 2, axis: 'y', axisOrder: 1},
-              {key: 'stdDeviation', name: 'Std dev', borderColor: stdDevColor, round: 4, axis: 'y2', axisOrder: 10},
-              {
-                key: 'accBadges', keys: [
-                  {key: 'SS+', name: 'SS+', backgroundColor: ssPlusColor, axisOrder: 2},
-                  {key: 'SS', name: 'SS', backgroundColor: ssColor, axisOrder: 3},
-                  {key: 'S+', name: 'S+', backgroundColor: sPlusColor, axisOrder: 4},
-                  {key: 'S', name: 'S', backgroundColor: sColor, axisOrder: 5},
-                  {key: 'A', name: 'A', backgroundColor: aColor, axisOrder: 5},
-                ], round: 0, axis: 'y1',
-              },
-            ]
-            : [],
-        )
-        .forEach(obj => {
-          const {key, secKey, keys, name, axis, ...options} = obj;
-          if (keys && Array.isArray(keys)) {
-            keys.forEach(obj => {
-              const {key: innerKey, name, ...innerOptions} = obj;
-              const fieldData = dayTimestamps.map(x => ({x, y: additionalHistory?.[x]?.[key]?.[innerKey] ?? 0}));
-
-              datasets.push({
-                ...options,
-                ...innerOptions,
-                yAxisID: axis,
-                label: name,
-                data: fieldData,
-                fill: true,
-                borderWidth: 2,
-                pointRadius: 1,
-                cubicInterpolationMode: 'monotone',
-                tension: 0.4,
-                type: 'bar',
-                stack: key,
-                spanGaps: true,
-                segment: {
-                  borderWidth: ctx => skipped(ctx, 1),
-                  borderDash: ctx => skipped(ctx, [6, 6]),
-                },
-              });
-            })
-          } else {
-            const fieldData = dayTimestamps.map(x => ({x, y: additionalHistory?.[x]?.[key] ?? null}));
-
-            datasets.push({
-              ...options,
-              yAxisID: axis,
-              label: name,
-              data: fieldData,
-              fill: false,
-              borderWidth: 2,
-              pointRadius: 1,
-              cubicInterpolationMode: 'monotone',
-              tension: 0.4,
-              type: 'line',
-              spanGaps: true,
-              segment: {
-                borderWidth: ctx => skipped(ctx, 1),
-                borderDash: ctx => skipped(ctx, [6, 6]),
-              },
-            });
-          }
         });
-    }
+      });
 
-    if (!chart)
-    {
+    if (!chart) {
       chart = new Chart(
-            canvas,
-            {
-              type: 'line',
-              data: {datasets},
-              options: {
-                responsive: true,
-                maintainAspectRatio: false,
-                layout: {
-                  padding: {
-                    right: 0
-                  }
-                },
-                interaction: {
-                  mode: 'index',
-                  intersect: false,
-                },
-                plugins: {
-                  legend: {
-                    display: true,
-                    onClick: onLegendClick,
+        canvas,
+        {
+          type: 'line',
+          data: {datasets},
+          options: {
+            responsive: true,
+            maintainAspectRatio: false,
+            layout: {
+              padding: {
+                right: 0,
+              },
+            },
+            interaction: {
+              mode: 'index',
+              intersect: false,
+            },
+            plugins: {
+              legend: {
+                display: true,
+                onClick: onLegendClick,
+              },
+              tooltip: {
+                position: 'nearest',
+                callbacks: {
+                  title(ctx) {
+                    if (!ctx?.[0]?.raw) return '';
+
+                    const nextDayDate = DateTime.fromMillis(ctx[0].raw?.x).plus({days: 1}).toJSDate();
+                    const nextDayDateFormatted = nextDayDate > new Date() ? 'now' : formatDate(nextDayDate, 'short', 'short');
+
+                    return `${formatDate(new Date(ctx[0].raw?.x), 'short', 'short')} - ${nextDayDateFormatted}`;
                   },
-                  tooltip: {
-                    position: 'nearest',
-                    callbacks: {
-                      title(ctx) {
-                        if (!ctx?.[0]?.raw) return '';
+                  label(ctx) {
+                    switch (ctx.dataset.label) {
+                      case 'SS+':
+                      case 'SS':
+                      case 'S+':
+                      case 'S':
+                      case 'A':
+                        return ` ${ctx.dataset.label}: ${formatNumber(ctx.parsed.y, ctx.dataset.round)}`
 
-                        const nextDayDate = DateTime.fromMillis(ctx[0].raw?.x).plus({days: 1}).toJSDate();
-                        const nextDayDateFormatted = nextDayDate > new Date() ? 'now' : formatDate(nextDayDate, 'short', 'short');
-
-                        return `${formatDate(new Date(ctx[0].raw?.x), 'short', 'short')} - ${nextDayDateFormatted}`;
-                      },
-                      label(ctx) {
-                        switch (ctx.dataset.label) {
-                          case 'SS+':
-                          case 'SS':
-                          case 'S+':
-                          case 'S':
-                          case 'A':
-                            return ` ${ctx.dataset.label}: ${formatNumber(ctx.parsed.y, ctx.dataset.round)}`
-
-                          default:
-                            return ` ${ctx.dataset.label}: ${formatNumber(ctx.parsed.y, ctx.dataset.round)}%`;
-                        }
-                      },
+                      default:
+                        return ` ${ctx.dataset.label}: ${formatNumber(ctx.parsed.y, ctx.dataset.round)}%`;
                     }
                   },
                 },
-                scales: {
-                  x: xAxis,
-                  ...yAxes,
-                },
               },
             },
-          );
-    }
-    else {
+            scales: {
+              x: xAxis,
+              ...yAxes,
+            },
+          },
+        },
+      );
+    } else {
       chart.data = {datasets};
       chart.options.scales = {x: xAxis, ...yAxes};
       chart.update();
@@ -305,25 +236,12 @@
   let debouncedChartHash = null;
   const debounceChartHash = debounce(chartHash => debouncedChartHash = chartHash, CHART_DEBOUNCE);
 
-  $: refreshPlayerHistory(playerId);
-
-  $: additionalHistory = playerHistory && playerHistory.length
-    ? playerHistory.reduce((cum, h) => {
-      const time = toBlMidnight(h.ssDate)?.getTime()
-      if (!time) return cum;
-
-      const history = {[time]: {averageRankedAccuracy: h. averageRankedAccuracy, avgAcc: h.avgAcc, medianAcc: h.medianAcc, stdDeviation:h.stdDeviation, accBadges: h.accBadges}};
-
-      return {...cum, ...history};
-    }, {})
-    : null;
-
-  $: chartHash = calcHistoryHash(rankHistory, additionalHistory);
+  $: chartHash = calcHistoryHash(statsHistory);
   $: debounceChartHash(chartHash)
-  $: if (debouncedChartHash) setupChart(debouncedChartHash, canvas)
+  $: if (debouncedChartHash) setupChart(debouncedChartHash, canvas);
 </script>
 
-{#if rankHistory && rankHistory.length}
+{#if statsHistory?.averageAccuracy?.length}
   <section class="chart" style="--height: {height}">
     <canvas class="chartjs" bind:this={canvas} height={parseInt(height,10)}></canvas>
   </section>
