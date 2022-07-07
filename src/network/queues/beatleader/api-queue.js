5174e92873b7fe100e807d1cb3bea7fa0ec4a97a
motzel <motzel@o2.pl>
2022-07-07 19:06:36 +0200

    feat: make the player's score always visible on the leaderboard

--- src/network/queues/beatleader/api-queue.js
+++ src/network/queues/beatleader/api-queue.js
@@ -14,6 +14,7 @@ export const BL_API_PLAYER_INFO_URL = BL_API_URL + 'player/${playerId}';
 export const BL_API_SCORES_URL = BL_API_URL + 'player/${playerId}/scores?page=${page}&sortBy=${sort}&order=${order}&search=${search}&diff=${diff}&type=${songType}&stars_from=${starsFrom}&stars_to=${starsTo}';
 export const BL_API_FRIENDS_SCORES_URL = BL_API_URL + 'user/friendScores?page=${page}&sortBy=${sort}&order=${order}&search=${search}&diff=${diff}&type=${songType}&stars_from=${starsFrom}&stars_to=${starsTo}&count=${count}';
 export const BL_API_SCORE_STATS_URL = BL_API_URL + 'score/statistic/${scoreId}'
+export const BL_API_PLAYER_SCORE_URL = BL_API_URL + 'score/${playerId}/${hash}/${diff}/${type}'
 export const BL_API_SCORES_HISTOGRAM_URL = BL_API_URL + 'player/${playerId}/histogram?sortBy=${sort}&order=${order}&search=${search}&diff=${diff}&type=${songType}&stars_from=${starsFrom}&stars_to=${starsTo}&batch=${batch}&count=${count}';
 export const BL_API_FIND_PLAYER_URL = BL_API_URL + 'players?search=${query}'
 export const BL_API_RANKING_URL = BL_API_URL + 'players?page=${page}&sortBy=${sortBy}&order=${order}&countries=${countries}&friends=${friends}&search=${search}&platform=${platform}&role=${role}&hmd=${hmd}&pp_range=${pp_range}&score_range=${score_range}'
@@ -39,6 +40,115 @@ export const BL_API_MINIRANKINGS_URL = BL_API_URL + 'minirankings?rank=${rank}&c
 export const STEAM_API_PROFILE_URL = STEAM_API_URL + '/ISteamUser/GetPlayerSummaries/v0002/?key=${steamKey}&steamids=${playerId}'
 export const STEAM_API_GAME_INFO_URL = STEAM_API_URL + '/IPlayerService/GetRecentlyPlayedGames/v0001/?key=${steamKey}&steamid=${playerId}'
 
+export const processLeaderboardScore = s => {
+  if (!s) return null;
+
+  let ret = {player: {playerInfo: {countries: []}}, score: {lastUpdated: new Date()}};
+
+  ret.score.rank = s?.rank;
+
+  const player = s.player;
+  let country = player.country.toUpperCase();
+  ret.player.playerInfo.country = country
+  ret.player.playerInfo.countries.push({country, rank: player.countryRank});
+  ret.player.playerInfo.avatar = player.avatar;
+  ret.player.playerInfo.allTime = player.allTime;
+  ret.player.playerInfo.lastTwoWeekTime = player.lastTwoWeekTime;
+
+  ret.player.name = player.name;
+  ret.player.name = ret.player.name ? ret.player.name.trim().replace('&#039;', "'") : null;
+  ret.player.playerId = player.id;
+  ret.player.playerId = ret.player.playerId ? ret.player.playerId.trim() : null;
+
+  ret.player.clans = player?.clans ?? null;
+
+  ret.score.score = s.modifiedScore;
+
+  ret.score.timeSetString = formatDateRelative(dateFromUnix(s.timeset));
+  ret.score.timeSet = s.timeset;
+  if (ret.score.timeSetString) ret.score.timeSetString = ret.score.timeSetString.trim();
+
+  ret.score.mods = s.modifiers && s.modifiers.length ? s.modifiers.split(',').filter(m => m && m.trim().length) : null;
+
+  ret.score.pp = s.pp;
+  ret.score.acc = s.accuracy * 100;
+
+  ret.score.id = s?.id ?? null;
+
+  return ret;
+}
+const processLeaderboardScores = response => response?.map(processLeaderboardScore) ?? null;
+const processLeaderboard = (leaderboardId, page, respons) => {
+
+  let led = respons.body;
+
+  const diffs = led?.song?.difficulties.map(a => {
+    let leaderboardId = led.song.id + "" + a.value + "" + a.mode;
+    let diffAndType = {diff: a.difficultyName, type: a.modeName};
+    let color = getDiffColor(diffAndType);
+    return {name: diffAndType.diff.replace('Plus', '+'), type: diffAndType.type, leaderboardId, color};
+  }) ?? null;
+
+  const currentDiff = led.difficulty;
+
+  let diff = null;
+  let diffInfo = null;
+  if (currentDiff) {
+    diffInfo = {diff: currentDiff.difficultyName, type: currentDiff.modeName};
+    diff = diffInfo.diff;
+  }
+
+  const songInfo = [
+    {id: 'hash', value: led?.song?.hash},
+    {id: 'id', value: led?.song?.id},
+    {id: 'scores', value: led?.plays},
+    {id: 'status', value: currentDiff?.ranked ? "Ranked" : "Not Ranked"},
+    {id: 'totalScores', value: led?.plays},
+    {id: 'notes', value: currentDiff?.notes},
+    {id: 'bpm', value: currentDiff?.bpm},
+    {id: 'stars', value: currentDiff?.stars},
+    {id: 'levelAuthorName', value: led?.song?.mapper},
+    {id: 'authorName', value: led?.song?.author},
+    {id: 'duration', value: led?.song?.duration},
+    {id: 'name', value: led?.song?.name}]
+    .reduce((cum, sid) => {
+      let value = sid.value;
+
+      if (value !== null && ['scores', 'totalScores', 'bpm', 'notes', 'stars', 'status'].includes(sid.id)) {
+
+        if (value !== null) {
+          cum.stats[sid.id] = value;
+        }
+
+        return cum;
+      }
+      if (value !== null) cum[sid.id] = value;
+
+      return cum;
+    }, {imageUrl: led?.song?.coverImage, stats: {}});
+
+  const {stats, ...song} = songInfo;
+  const leaderboard = {leaderboardId, song, diffInfo, stats};
+
+  const totalItems = led.plays;
+  const pageQty = 10;
+
+  const scores = processLeaderboardScores(led?.scores);
+
+  // let diffChartText = getFirstRegexpMatch(/'difficulty',\s*([0-9.,\s]+)\s*\]/, response.body.innerHTML)
+  let diffChart = null;//(diffChartText ? diffChartText : '').split(',').map(i => parseFloat(i)).filter(i => i && !isNaN(i));
+
+  return {
+    diffs,
+    leaderboard,
+    diffChart,
+    page,
+    pageQty,
+    totalItems,
+    scores
+  }
+}
+
 export default (options = {}) => {
   const queue = createQueue(options);
 
@@ -58,6 +168,8 @@ export default (options = {}) => {
   const scoresHistogram = async (playerId, params = {sort: 'date', order: 'desc', filter: {}}, priority = PRIORITY.FG_LOW, options = {}) => fetchScores(substituteVars(BL_API_SCORES_HISTOGRAM_URL, {...params, ...(params?.filters ?? {})}), playerId, priority, options);
 
   const scoreStats = async (scoreId, priority = PRIORITY.FG_LOW, options = {}) => fetchJson(substituteVars(BL_API_SCORE_STATS_URL, {scoreId: encodeURIComponent(scoreId)}), options, priority);
+
+  const playerScore = async (playerId, hash, diff, type, priority = PRIORITY.FG_LOW, options = {}) => fetchJson(substituteVars(BL_API_PLAYER_SCORE_URL, {playerId, hash, diff, type}), options, priority);
 
   const findPlayer = async (query, priority = PRIORITY.FG_LOW, options = {}) => fetchJson(substituteVars(BL_API_FIND_PLAYER_URL, {query: encodeURIComponent(query)}), options, priority);
 
@@ -106,115 +218,6 @@ export default (options = {}) => {
 
   const accGraph = async (playerId, priority = PRIORITY.FG_LOW, options = {}) => fetchJson(substituteVars(BL_API_ACC_GRAPH_URL, {player: playerId}), options, priority);
 
-  const processLeaderboardScores = response => {
-    return response.map(s => {
-      let ret = {player: {playerInfo: {countries: []}}, score: {lastUpdated: new Date()}};
-
-      ret.score.rank = s.rank;
-
-      const player = s.player;
-      let country = player.country.toUpperCase();
-      ret.player.playerInfo.country = country
-      ret.player.playerInfo.countries.push({country, rank: player.countryRank});
-      ret.player.playerInfo.avatar = player.avatar;
-      ret.player.playerInfo.allTime = player.allTime;
-      ret.player.playerInfo.lastTwoWeekTime = player.lastTwoWeekTime;
-
-      ret.player.name = player.name;
-      ret.player.name = ret.player.name ? ret.player.name.trim().replace('&#039;', "'") : null;
-      ret.player.playerId = player.id;
-      ret.player.playerId = ret.player.playerId ? ret.player.playerId.trim() : null;
-
-      ret.player.clans = player?.clans ?? null;
-
-      ret.score.score = s.modifiedScore;
-
-      ret.score.timeSetString = formatDateRelative(dateFromUnix(s.timeset));
-      ret.score.timeSet = s.timeset;
-      if (ret.score.timeSetString) ret.score.timeSetString = ret.score.timeSetString.trim();
-
-      ret.score.mods = s.modifiers && s.modifiers.length ? s.modifiers.split(',').filter(m => m && m.trim().length) : null;
-
-      ret.score.pp = s.pp;
-      ret.score.acc = s.accuracy * 100;
-
-      ret.score.id = s?.id ?? null;
-
-      return ret;
-    });
-  }
-
-  const processLeaderboard = (leaderboardId, page, respons) => {
-
-    let led = respons.body;
-
-    const diffs = led.song.difficulties.map(a => {
-      let leaderboardId = led.song.id + "" + a.value + "" + a.mode;
-      let diffAndType = {diff: a.difficultyName, type: a.modeName};
-      let color = getDiffColor(diffAndType);
-      return {name: diffAndType.diff.replace('Plus', '+'), type: diffAndType.type, leaderboardId, color};
-    });
-
-    const currentDiff = led.difficulty;
-
-    let diff = null;
-    let diffInfo = null;
-    if (currentDiff) {
-      diffInfo = {diff: currentDiff.difficultyName, type: currentDiff.modeName};
-      diff = diffInfo.diff;
-    }
-
-    const songInfo = [
-      {id: 'hash', value: led.song.hash},
-      {id: 'id', value: led.song.id},
-      {id: 'scores', value: led.plays},
-      {id: 'status', value: currentDiff.ranked ? "Ranked" : "Not Ranked"},
-      {id: 'totalScores', value: led.plays},
-      {id: 'notes', value: currentDiff.notes},
-      {id: 'bpm', value: currentDiff.bpm},
-      {id: 'stars', value: currentDiff.stars},
-      {id: 'levelAuthorName', value: led.song.mapper},
-      {id: 'authorName', value: led.song.author},
-      {id: 'duration', value: led.song.duration},
-      {id: 'name', value: led.song.name}]
-      .reduce((cum, sid) => {
-        let value = sid.value;
-
-        if (value !== null && ['scores', 'totalScores', 'bpm', 'notes', 'stars', 'status'].includes(sid.id)) {
-
-          if (value !== null) {
-            cum.stats[sid.id] = value;
-          }
-
-          return cum;
-        }
-        if (value !== null) cum[sid.id] = value;
-
-        return cum;
-      }, {imageUrl: led.song.coverImage, stats: {}});
-
-    const {stats, ...song} = songInfo;
-    const leaderboard = {leaderboardId, song, diffInfo, stats};
-
-    const totalItems = led.plays;
-    const pageQty = 10;
-
-    const scores = processLeaderboardScores(led.scores);
-
-    // let diffChartText = getFirstRegexpMatch(/'difficulty',\s*([0-9.,\s]+)\s*\]/, response.body.innerHTML)
-    let diffChart = null;//(diffChartText ? diffChartText : '').split(',').map(i => parseFloat(i)).filter(i => i && !isNaN(i));
-
-    return {
-      diffs,
-      leaderboard,
-      diffChart,
-      page,
-      pageQty,
-      totalItems,
-      scores
-    }
-  }
-
   const leaderboard = async (leaderboardId, page = 1, filters = {}, priority = PRIORITY.FG_LOW, options = {}) =>
     fetchJson(substituteVars(BL_API_LEADERBOARD_URL, {leaderboardId, page, ...filters}, true, true), {...options, credentials: 'include'}, priority)
       .then(r => {
@@ -239,6 +242,7 @@ export default (options = {}) => {
     scores,
     scoreStats,
     scoresHistogram,
+    playerScore,
     leaderboards,
     leaderboard,
     clans,
