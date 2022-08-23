93038faa2e719d84082b31ea63df7e21c9a95664
motzel <motzel@o2.pl>
2022-08-23 08:22:42 +0200

    fix: criteria met counts on the RT dashboard

--- src/pages/RtDashboard.svelte
+++ src/pages/RtDashboard.svelte
@@ -497,7 +497,7 @@
 							carry.nominated += !!diff?.nominated || !!diff?.qualified ? 1 : 0;
 							carry.qualified += !!diff?.qualified ? 1 : 0;
 							carry.mapperAllowed += diff?.qualification?.mapperAllowed ? 1 : 0;
-							carry.criteriaMet += diff?.qualification?.criteriaMet !== 0 ? 1 : 0;
+							carry.criteriaMet += diff?.qualification?.criteriaMet === 1 ? 1 : 0;
 							carry.approved += diff?.qualification?.approved ? 1 : 0;
 							carry.votesTotal += diff?.votes?.length ?? 0;
 							carry.votesPositive += diff?.votesPositive ?? 0;
