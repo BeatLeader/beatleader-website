295022ca2759c7095b13cdd7a124a2aabd929a02
NSGolova <golova@golova.dev>
2022-09-21 14:41:42 +0100

    Fixed opening tabs in the background just from the CTRL

--- public/assets/fix-ctrl-click.js
+++ public/assets/fix-ctrl-click.js
@@ -1,42 +1,27 @@
 !(function () {
-    let ctrlPressState = false;
-    window.addEventListener("keydown", (e) => {
-        if (e.key === "Control") ctrlPressState = true
-    })
+	let ctrlPressState = false;
 
-    window.addEventListener("keyup", (e) => {
-        if (e.key === "Control") ctrlPressState = false
-    })
+	window.addEventListener('mousedown', function (e) {
+		ctrlPressState = e.ctrlKey;
+	});
 
-    function openInBackground(url) {
-        const a = document.createElement("a");
-        a.href = url;
-        const evt = document.createEvent("MouseEvents");
-        evt.initMouseEvent(
-            "click",
-            true,
-            true,
-            window,
-            0,
-            0,
-            0,
-            0,
-            0,
-            true,
-            false,
-            false,
-            false,
-            0,
-            null
-        );
-        a.dispatchEvent(evt);
-    }
+	function openInBackground(url) {
+		const a = document.createElement('a');
+		a.href = url;
+		const evt = document.createEvent('MouseEvents');
+		evt.initMouseEvent('click', true, true, window, 0, 0, 0, 0, 0, true, false, false, false, 0, null);
+		a.dispatchEvent(evt);
+	}
 
-    window.addEventListener("load", () => {
-        window.history.oldPushState = window.history.pushState;
-        window.history.pushState = (...args) => {
-            if (ctrlPressState) openInBackground(args[2])
-            else window.history.oldPushState(...args)
-        }
-    })
-})()
+	window.addEventListener('load', () => {
+		window.history.oldPushState = window.history.pushState;
+		window.history.pushState = (...args) => {
+			if (ctrlPressState) {
+				openInBackground(args[2]);
+				ctrlPressState = false;
+			} else {
+				window.history.oldPushState(...args);
+			}
+		};
+	});
+})();
