fe76ae7ae1b6c2022732685d3a61e34555f9b681
motzel <motzel@o2.pl>
2022-09-25 15:42:20 +0200

    feat: add a button above the avatar in edit mode

--- src/components/Player/AvatarOverlayIcons.svelte
+++ src/components/Player/AvatarOverlayIcons.svelte
@@ -189,6 +189,14 @@
 				title="{beatsaverSocial.user} mapper" />
 		{/if}
 	</nav>
+
+	{#if editModel && !editModel.avatarOverlayEdit}
+		<div class="imageInput" on:click={() => (editModel.avatarOverlayEdit = true)}>
+			<span class="imageChange">
+				<h3 class="changeLabel">Change</h3>
+			</span>
+		</div>
+	{/if}
 {/if}
 
 <style>
@@ -278,5 +286,34 @@
 
 	nav :global(.twitch):hover {
 		transform: scale(1.2);
+	}
+
+	.imageInput {
+		cursor: pointer;
+		display: flex;
+		position: absolute;
+		width: 100px;
+		height: 100px;
+		top: calc(50% - 48px);
+		left: calc(50% - 48px);
+		align-items: center;
+		z-index: 6;
+	}
+
+	.imageChange {
+		transition: opacity 0.2s ease-in-out;
+		background-color: rgba(32, 33, 36, 0.6);
+		height: 33%;
+		left: 0;
+		opacity: 1;
+		position: absolute;
+		right: 0;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.changeLabel {
+		position: absolute;
 	}
 </style>
