64756d843cd1f022506de3a4c3e35866fe1b191e
NSGolova <golova@golova.dev>
2022-11-22 02:24:16 +0000

    Fix clan badge color invert for RGBA hex

--- src/components/Player/ClanBadges.svelte
+++ src/components/Player/ClanBadges.svelte
@@ -12,14 +12,15 @@
 		if (hex.length === 3) {
 			hex = hex[0] + hex[0] + hex[1] + hex[1] + hex[2] + hex[2];
 		}
-		if (hex.length !== 6) {
-			throw new Error('Invalid HEX color.');
+		if (hex.length === 6) {
+			hex = hex + 'FF';
 		}
 		var r = parseInt(hex.slice(0, 2), 16),
 			g = parseInt(hex.slice(2, 4), 16),
-			b = parseInt(hex.slice(4, 6), 16);
+			b = parseInt(hex.slice(4, 6), 16),
+			a = parseInt(hex.slice(6, 8), 16);
 		// https://stackoverflow.com/a/3943023/112731
-		return r * 0.299 + g * 0.687 + b * 0.114 > 186 ? '#000000' : '#FFFFFF';
+		return r * 0.299 + g * 0.687 + b * 0.114 > 186 && a > 128 ? '#000000' : '#FFFFFF';
 	}
 
 	$: clans = player?.clans ?? null;
