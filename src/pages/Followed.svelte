2a9fca4ad1d48d1ccd9b398b2ed2da5e280c2596
motzel <motzel@o2.pl>
2023-03-11 21:01:15 +0100

    feat: add option to sort followed

--- src/pages/Followed.svelte
+++ src/pages/Followed.svelte
@@ -3,16 +3,24 @@
 	import {fade} from 'svelte/transition';
 	import ssrConfig from '../ssr-config';
 	import {PLAYERS_PER_PAGE} from '../utils/beatleader/consts';
-	import createAccountStore from '../stores/beatleader/account';
 	import followed from '../stores/beatleader/followed';
+	import createAccountStore from '../stores/beatleader/account';
+	import {opt} from '../utils/js';
 	import ContentBox from '../components/Common/ContentBox.svelte';
 	import PlayerNameWithFlag from '../components/Common/PlayerNameWithFlag.svelte';
 	import Avatar from '../components/Common/Avatar.svelte';
 	import Value from '../components/Common/Value.svelte';
 
+	document.body.classList.add('slim');
+
 	const account = createAccountStore();
 
-	document.body.classList.add('slim');
+	const sortOptions = [
+		{key: 'name', label: 'Sort by Name', type: 'string'},
+		{key: 'playerInfo.rank', label: 'Sort by Global Rank'},
+		{key: 'playerInfo.countries.0.rank', label: 'Sort by Country Rank'},
+	];
+	let sortBy = sortOptions[0];
 
 	function getCountryRankingUrl(countryObj) {
 		const rank = countryObj?.rankValue ?? countryObj?.rank ?? null;
@@ -44,72 +52,93 @@
 		}));
 	}
 
-	$: followedSorted = ($followed ?? []).sort((a, b) => a?.name.localeCompare(b?.name));
+	$: followedSorted =
+		(sortBy,
+		($followed ?? []).sort((a, b) => {
+			let [first, second] = sortBy.reverse === true ? [b, a] : [a, b];
+
+			return sortBy.type === 'string'
+				? opt(first, sortBy.key, '').localeCompare(opt(second, sortBy.key, ''))
+				: opt(first, sortBy.key, 0) - opt(second, sortBy.key, 0);
+		}));
 </script>
 
 <svelte:head>
 	<title>Followed - {ssrConfig.name}</title>
 </svelte:head>
 
-<article transition:fade>
+<article>
 	<ContentBox>
-		<h1 class="title is-3">Followed</h1>
+		<h1 class="title is-3">
+			<span>Followed</span>
+			<select bind:value={sortBy}>
+				{#each sortOptions as option}
+					<option value={option}>{option.label}</option>
+				{/each}
+			</select>
+		</h1>
 
 		{#if $account.loading}
 			<p>Loading...</p>
 		{:else if followedSorted?.length}
-			<section class="grid">
-				{#each followedSorted as f}
-					<a href={`/u/${f.playerId}`} on:click|preventDefault={() => navigate(`/u/${f.playerId}`)}>
-						<ContentBox>
-							{#if f?.profileSettings?.profileCover}
-								<div class="profile-background" style:background-image={`url(${f.profileSettings.profileCover})`} />
-							{/if}
+			{#key sortBy.key}
+				<section class="grid">
+					{#each followedSorted as f, idx (f?.playerId)}
+						<a
+							href={`/u/${f.playerId}`}
+							on:click|preventDefault={() => navigate(`/u/${f.playerId}`)}
+							in:fade={{delay: idx * 20, duration: 200}}
+							out:fade={{duration: 50}}>
+							<ContentBox>
+								{#if f?.profileSettings?.profileCover}
+									<div class="profile-background" style:background-image={`url(${f.profileSettings.profileCover})`} />
+								{/if}
 
-							<div class="avatar-cell">
-								<Avatar player={f} />
-							</div>
+								<div class="avatar-cell">
+									<Avatar player={f} />
+								</div>
 
-							<PlayerNameWithFlag player={f} disablePopover={true} hideFlag={true} />
+								<PlayerNameWithFlag player={f} disablePopover={true} hideFlag={true} />
 
-							<section class="stats">
-								<a
-									style="flex: none"
-									href={`/ranking/${Math.floor(((f?.playerInfo?.rank ?? 1) - 1) / PLAYERS_PER_PAGE) + 1}`}
-									on:click|preventDefault={() => navigateToGlobalRanking(f?.playerInfo?.rank ?? 1)}
-									title="Go to global ranking"
-									class="clickable">
-									<i class="fas fa-globe-americas" />
-
-									<Value value={f?.playerInfo?.rank} prefix="#" digits={0} zero="#0" inline={true} reversePrevSign={true} />
-								</a>
-
-								{#each getPlayerCountries(f?.playerInfo) as country}
+								<section class="stats">
 									<a
 										style="flex: none"
-										href={getCountryRankingUrl(country)}
-										on:click|preventDefault={() => navigateToCountryRanking(country)}
-										title="Go to country ranking"
+										href={`/ranking/${Math.floor(((f?.playerInfo?.rank ?? 1) - 1) / PLAYERS_PER_PAGE) + 1}`}
+										on:click|preventDefault={() => navigateToGlobalRanking(f?.playerInfo?.rank ?? 1)}
+										title="Go to global ranking"
 										class="clickable">
-										<img
-											src={`/assets/flags/${
-												country && country.country && country.country.toLowerCase ? country.country.toLowerCase() : ''
-											}.png`}
-											class="countryIcon"
-											alt={country?.country} />
+										<i class="fas fa-globe-americas" />
 
-										<Value value={country.rank} prefix="#" digits={0} zero="#0" inline={true} reversePrevSign={true} />
+										<Value value={f?.playerInfo?.rank} prefix="#" digits={0} zero="#0" inline={true} reversePrevSign={true} />
 									</a>
-								{/each}
 
-								<span class="pp">
-									<Value value={f?.playerInfo?.pp} suffix="pp" inline={true} zero="0pp" />
-								</span>
-							</section>
-						</ContentBox>
-					</a>
-				{/each}
-			</section>
+									{#each getPlayerCountries(f?.playerInfo) as country}
+										<a
+											style="flex: none"
+											href={getCountryRankingUrl(country)}
+											on:click|preventDefault={() => navigateToCountryRanking(country)}
+											title="Go to country ranking"
+											class="clickable">
+											<img
+												src={`/assets/flags/${
+													country && country.country && country.country.toLowerCase ? country.country.toLowerCase() : ''
+												}.png`}
+												class="countryIcon"
+												alt={country?.country} />
+
+											<Value value={country.rank} prefix="#" digits={0} zero="#0" inline={true} reversePrevSign={true} />
+										</a>
+									{/each}
+
+									<span class="pp">
+										<Value value={f?.playerInfo?.pp} suffix="pp" inline={true} zero="0pp" />
+									</span>
+								</section>
+							</ContentBox>
+						</a>
+					{/each}
+				</section>
+			{/key}
 		{:else}
 			<p>None followed, add someone.</p>
 		{/if}
@@ -117,6 +146,15 @@
 </article>
 
 <style>
+	h1 {
+		display: flex;
+		justify-content: space-between;
+	}
+
+	select {
+		font-size: 0.875rem;
+	}
+
 	.grid {
 		display: grid;
 		grid-template-columns: repeat(auto-fill, minmax(23rem, 1fr));
