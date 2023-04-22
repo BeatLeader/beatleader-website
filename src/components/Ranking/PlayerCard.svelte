1bdd479e3c443cdd0c1e75e94273a1fd6742dee2
NSGolova <golova@golova.dev>
2023-04-22 14:31:38 +0100

    Fixed clan pages

--- src/components/Ranking/PlayerCard.svelte
+++ src/components/Ranking/PlayerCard.svelte
@@ -132,7 +132,7 @@
 	</div>
 	<div class="steam-and-pp">
 		<div>
-			{#if currentFilters.sortBy == 'dailyImprovements'}
+			{#if currentFilters?.sortBy == 'dailyImprovements'}
 				<span style="color: {!value ? 'red' : 'green'}">{value ? value + ' improved' : 'Carbon positive'}</span>
 			{:else if valueProps.isText}
 				{value}
