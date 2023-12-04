0bd75308849d6de52baaf75c74158df1f94a9d0d
LightAi39 <infowesselvanhemert@gmail.com>
2023-12-04 17:43:01 +0100

    switching to pc dont mind me

--- src/components/Replayed/ReplayedCard.svelte
+++ src/components/Replayed/ReplayedCard.svelte
@@ -1,22 +1,34 @@
 <script>
-	import { fade } from 'svelte/transition';
+	import {fade} from 'svelte/transition';
 	import Button from '../Common/Button.svelte';
 	import {navigate} from 'svelte-routing/src/history';
+	import Reveal from '../Common/Reveal.svelte';
 
 	export let title = '';
-	export let body = '';
+	export let subText = '';
+	export let stats;
 	export let imageUrl = '';
 	export let targetUrl;
 	export let buttons = [];
 	export let active = false;
 	export let clickAction;
-  export let nextAction;
+	export let nextAction;
+	export let introBgColor = 'rgba(201, 52, 157, 1)';
+
+	let mainStat = stats?.entries[0];
+	let revealed = false;
+
+	buttons.push({
+		text: 'Next',
+		type: 'primary',
+		function: nextAction,
+	});
 
 	let cinematicsCanvas;
 
 	function handleCardClick() {
 		if (active) {
-			if (targetUrl) navigate(targetUrl);
+			reveal();
 		} else {
 			clickAction();
 		}
@@ -35,8 +47,17 @@
 		}
 	}
 
+	function reveal() {
+		revealed = true;
+	}
+
 	$: drawCinematics(cinematicsCanvas, imageUrl);
 	$: if (buttons.length > 3) buttons = buttons.slice(0, 3);
+	$: {
+		stats?.entries.sort((a, b) => a.index - b.index);
+		mainStat = stats?.entries[0];
+		imageUrl = mainStat?.imageUrl;
+	}
 </script>
 
 <!-- svelte-ignore a11y-no-static-element-interactions -->
@@ -44,7 +65,7 @@
 <div class="grid-item" class:active>
 	<div class="card" on:click={handleCardClick} on:mouseenter class:active>
 		<div class="cinematics">
-			<div class="cinematics-canvas" class:active>
+			<div class="cinematics-canvas" class:active={revealed}>
 				<canvas bind:this={cinematicsCanvas} style="position: absolute; width: 100%; height: 100%; opacity: 0" />
 			</div>
 		</div>
@@ -52,10 +73,22 @@
 			<div class="background" style="background-image: url({imageUrl});" />
 		</div>
 
-		<div class="content">
-			<h1>{title}</h1>
-			<p>{body}</p>
+		<div class="intro-card" style="background-color:{introBgColor};" class:inactive={revealed}>
+			<div class="intro-card-content">
+				<div class="header">
+					<h1>{title}</h1>
+					<p>{subText}</p>
+				</div>
 
+				{#if stats?.type === 'mapList'}
+					<img src={mainStat.imageUrl} alt={mainStat.title} />
+					<h2>{mainStat.title}</h2>
+					<h3>{mainStat.mapper}</h3>
+				{/if}
+			</div>
+		</div>
+
+		<div class="content">
 			<div class="buttons" class:active>
 				{#each buttons as button}
 					<Button
@@ -63,6 +96,7 @@
 						url={button.url}
 						type={button.type}
 						on:click={() => {
+							if (button.function) button.function();
 							if (button.url) navigate(button.url);
 						}} />
 				{/each}
@@ -76,14 +110,104 @@
 		box-sizing: border-box;
 		display: flex;
 		width: 100%;
-		padding: 1em;
-    position: relative;
-    transition: padding 300ms ease;
+		padding: 0.5em;
+		position: relative;
+		transition: padding 300ms ease;
 	}
 
-  .grid-item.active {
-    padding: 0.5em;
-  }
+	.grid-item.active {
+		padding: 0.5em;
+	}
+
+	.intro-card {
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		position: absolute;
+		overflow: hidden;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		border-radius: 12px;
+		padding: 1em;
+		backdrop-filter: blur(1em);
+		z-index: 20;
+
+		color: white;
+		user-select: none;
+		text-shadow: 2px 2px 6px rgba(0, 0, 0, 0.85);
+		font-size: 3.5vh;
+
+		font-family: Noto Sans SC;
+		font-style: normal;
+		line-height: normal;
+		text-align: center;
+
+		transition: transform 300ms ease-in-out, opacity 300ms ease-in-out, backdrop-filter 300ms ease-in-out;
+	}
+
+	.intro-card::before {
+		left: 0;
+		right: 0;
+		top: 0;
+		bottom: 0;
+		position: absolute;
+		overflow: hidden;
+		border-radius: 12px;
+	}
+
+	.intro-card.inactive {
+		pointer-events: none;
+		opacity: 0;
+		backdrop-filter: blur(1em) opacity(0);
+		transform: translateX(-100%);
+	}
+
+	.intro-card h1 {
+		font-size: 100%;
+		font-weight: 700;
+	}
+
+	.intro-card h2 {
+		font-size: 80%;
+		font-weight: 700;
+	}
+
+	.intro-card h3 {
+		font-size: 60%;
+		font-weight: 600;
+	}
+
+	.intro-card p {
+		font-size: 50%;
+		font-weight: 400;
+	}
+
+	.intro-card img {
+		width: 50%;
+		justify-content: center;
+		align-self: center;
+		border-radius: 12px;
+		box-shadow: 0 2px 6px rgba(0, 0, 0, 0.85);
+		margin-bottom: 0.5em;
+	}
+
+	.intro-card-content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.intro-card .header {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 1em;
+	}
 
 	.card {
 		width: 100%;
@@ -120,16 +244,28 @@
 		transition: transform 600ms ease;
 		z-index: 0;
 		pointer-events: none;
+		filter: blur(0.25em);
 	}
 
-	.card.active:hover .background {
-		transform: scale(1.1);
+	.background::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(255, 255, 255, 0.15);
+		backdrop-filter: blur(0.25em);
 	}
 
-  .card:hover .background {
-    transform: scale(1.025);
-  }
-	.card:hover .cinematics-canvas {
+	.card.active .background {
+		transform: scale(1);
+	}
+
+	.card:hover .background {
+		transform: scale(1.025);
+	}
+	.card:hover .cinematics-canvas.active {
 		transform: scale(1.125);
 		filter: blur(5em) opacity(0.5) saturate(250%) brightness(120%);
 	}
@@ -147,19 +283,19 @@
 	}
 
 	.content h1 {
-    user-select: none;
+		user-select: none;
 	}
 
 	.content p {
 		color: white;
-    user-select: none;
+		user-select: none;
 	}
 
 	.buttons {
 		display: flex;
 		flex-direction: row;
 		flex-wrap: wrap-reverse;
-		justify-content: flex-end;
+		justify-content: center;
 		overflow: hidden;
 		row-gap: -0.25em;
 		column-gap: 0.5em;
@@ -190,7 +326,7 @@
 		pointer-events: none;
 		position: absolute;
 		top: 0;
-		transform: scale(1.05) translateZ(0);
+		transform: scale(0) translateZ(0);
 		width: 100%;
 		z-index: -1;
 		height: 100%;
@@ -198,6 +334,7 @@
 	}
 
 	.cinematics-canvas.active {
+		transform: scale(1.05) translateZ(0);
 		filter: blur(5em) opacity(0.5) saturate(250%);
 	}
 </style>
--- src/pages/Replayed.svelte
+++ src/pages/Replayed.svelte
@@ -1,29 +1,45 @@
 <script>
-	import { fade } from "svelte/transition";
-	import ContentBox from "../components/Common/ContentBox.svelte";
-	import FeaturedCarousel from "../components/Maps/FeaturedCarousel.svelte";
-	import ReplayedCard from "../components/Replayed/ReplayedCard.svelte";
-  import { onMount } from "svelte";
+	import {fade} from 'svelte/transition';
+	import ContentBox from '../components/Common/ContentBox.svelte';
+	import FeaturedCarousel from '../components/Maps/FeaturedCarousel.svelte';
+	import ReplayedCard from '../components/Replayed/ReplayedCard.svelte';
+	import {onMount} from 'svelte';
 
-  let cards = [
-    {
-      component: ReplayedCard,
-      props: {
-        title: 'Test Card',
+	let cards = [
+		{
+			component: ReplayedCard,
+			props: {
+				title: 'Your most fails',
+				subText: 'This map made you struggle the most',
+				stats: {
+					type: 'mapList',
+					entries: [
+						{
+							index: 0,
+							title: 'Villain Virus',
+							mapper: 'GalaxyMaster & GojiCrafter',
+							imageUrl: 'https://eu.cdn.beatsaver.com/46dae11677d6191ed03ad61286a09b9070be7800.jpg',
+						},
+					],
+				},
+				imageUrl: '/assets/landing-big.jpg',
+				buttons: [
+					{
+						text: 'More Info',
+						type: 'secondary',
+					},
+				],
+			},
+		},
+		{
+			component: ReplayedCard,
+			props: {
+				title: 'Test Card 2',
 				body: 'hi',
 				imageUrl: '/assets/landing-big.jpg',
-      }
-    },
-    {
-      component: ReplayedCard,
-      props: {
-        title: 'Test Card 2',
-				body: 'hi',
-				imageUrl: '/assets/landing-big.jpg',
-      }
-    }
-  ]
-
+			},
+		},
+	];
 </script>
 
 <svelte:head>
@@ -31,46 +47,42 @@
 </svelte:head>
 
 <section class="align-content">
-  <article class="page-content align-content" transition:fade|global>
-    <ContentBox cls="main-content-replayed">
-      <div class="items">
-
-        <FeaturedCarousel cards={cards} showFillerCards={false} height={"150%"} cardWidthRatio={0.95} showButtons/>
-      </div>
-
-    </ContentBox>
-  </article>
-
+	<article class="page-content align-content" transition:fade|global>
+		<ContentBox cls="main-content-replayed">
+			<div class="items">
+				<FeaturedCarousel {cards} showFillerCards={false} height={'1000%'} cardWidthRatio={0.95} showButtons />
+			</div>
+		</ContentBox>
+	</article>
 </section>
 
 <style>
-  .align-content {
+	.align-content {
 		display: flex;
 		justify-content: center !important;
 	}
 
-  :global(.main-content-replayed) {
-    aspect-ratio: 9 / 16;
-  }
+	:global(.main-content-replayed) {
+		aspect-ratio: 9 / 16;
+	}
 
 	.page-content {
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
 	}
 
 	article {
 		width: 100%;
-    height: 100%;
+		height: 100%;
 		overflow-x: visible;
 	}
 
-  .items {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    height: 100%;
-    overflow: hidden;
-  }
-
+	.items {
+		display: flex;
+		flex-direction: column;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+	}
 </style>
