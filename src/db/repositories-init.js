894cc6bb9421b8f08fa89803cc3a8055d6b531bd
NSGolova <golova@golova.dev>
2024-01-20 22:00:29 +0000

    Removed now obsolete Twitch ID fixes

--- src/db/repositories-init.js
+++ src/db/repositories-init.js
@@ -1,11 +1,10 @@
 import cacheRepository from './repository/cache';
 import keyValueRepository from './repository/key-value';
-import twitchRepository from './repository/twitch';
 import log from '../utils/logger';
 
 export default () => {
 	log.debug('Initialize DB repositories');
 
 	// initialize all repositories in order to create cache to sync
-	[cacheRepository, keyValueRepository, twitchRepository].map(repository => repository());
+	[cacheRepository, keyValueRepository].map(repository => repository());
 };
