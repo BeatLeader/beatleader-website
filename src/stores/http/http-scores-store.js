BeatLeader/beatleader-website

commit ec36211c73f7c0c1b7e88fc64ebef65812c843aa

Completely removed old Twitch integration

src/stores/http/http-scores-store.js
@@ -1,6 +1,5 @@
 import createHttpStore from './http-store';
 import beatMapsEnhancer from './enhancers/common/beatmaps';
-import twitchEnhancer from './enhancers/scores/twitch';
 import ppAttributionEnhancer from './enhancers/scores/pp-attribution';
 import {debounce} from '../../utils/debounce';
 import createApiScoresProvider from './providers/api-scores';
@@ -90,12 +89,12 @@ export default (
 			if (currentService !== 'accsaber') {
 				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => ppAttributionEnhancer(draft, currentPlayerId))
 					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow))
-					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => twitchEnhancer(draft, currentPlayerId)))
+					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow)))
 					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow));
 			} else {
 				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => beatMapsEnhancer(draft))
 					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow))
-					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => twitchEnhancer(draft, currentPlayerId)))
+					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow)))
 					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow));
 			}
 		}

src/App.svelte
@@ -16,7 +16,6 @@
 	import ClansPage from './pages/Clans.svelte';
 	import FollowedPage from './pages/Followed.svelte';
 	import PlayerPage from './pages/Player.svelte';
-	import TwitchPage from './pages/Twitch.svelte';
 	import NotFoundPage from './pages/NotFound.svelte';
 	import PrivacyPage from './pages/Privacy.svelte';
 	import AboutPage from './pages/About.svelte';
@@ -244,7 +243,6 @@
 					<Route path="/playlist/:id" let:params>
 						<PlaylistPage id={params.id} />
 					</Route>
-					<Route path="/twitch" component={TwitchPage} />
 					<Route path="/help" component={SupportPage} />
 					<Route path="/dashboard" component={DashboardPage} />
 					<Route path="/signin/*action" let:params>

src/components/Player/TwitchLinkModal.svelte
@@ -1,169 +1,0 @@
-<script>
-	import {createEventDispatcher} from 'svelte';
-	import createTwitchService from '../../services/twitch';
-	import Button from '../Common/Button.svelte';
-	import Dialog from '../Common/Dialog.svelte';
-	import {delay} from '../../utils/promise';
-	import {SsrHttpUnauthenticatedError, SsrHttpUnauthorizedError} from '../../network/errors';
-
-	export let playerId;
-	export let show = false;
-
-	const dispatch = createEventDispatcher();
-
-	const twitchService = createTwitchService();
-
-	let twitchProfile = null;
-
-	let twitchUserName = '';
-	let alreadySearched = false;
-	let isSearching = false;
-	let error = null;
-
-	async function onPlayerChanged(playerId) {
-		if (!playerId) return;
-
-		twitchProfile = await twitchService.getPlayerProfile(playerId);
-		if (twitchProfile && twitchProfile.login) {
-			twitchUserName = twitchProfile.login;
-
-			if (!twitchProfile.id) onFindUser();
-		}
-	}
-
-	function onCancel() {
-		dispatch('cancel');
-	}
-
-	async function onFindUser() {
-		if (!twitchUserName || !twitchUserName.length) return;
-
-		try {
-			isSearching = true;
-			alreadySearched = false;
-			twitchProfile = null;
-			error = null;
-
-			await delay(100);
-			twitchProfile = await twitchService.fetchProfile(twitchUserName);
-
-			alreadySearched = true;
-		} catch (err) {
-			if (err instanceof SsrHttpUnauthenticatedError || err instanceof SsrHttpUnauthorizedError) {
-				error =
-					'Twitch authentication error. It is likely that your Twitch token is invalid. Go to Settings and reconnect service with your Twitch account.';
-			} else {
-				error = 'An error occurred while trying to connect to Twitch.';
-			}
-		} finally {
-			isSearching = false;
-		}
-	}
-
-	function onTwitchUserNameKeyUp(e) {
-		if (e.code === 'Enter') {
-			e.preventDefault();
-
-			onFindUser();
-
-			return false;
-		}
-	}
-
-	$: onPlayerChanged(playerId);
-</script>
-
-{#if show}
-	<Dialog closeable={true} on:confirm={onCancel}>
-		<svelte:fragment slot="header">
-			<div class="header-title">Set up a Twitch profile</div>
-		</svelte:fragment>
-
-		<svelte:fragment slot="content">
-			<div class="search">
-				<input type="text" bind:value={twitchUserName} placeholder="Enter Twitch username..." on:keyup={onTwitchUserNameKeyUp} />
-				<Button iconFa="fas fa-search" type="primary" label="Search" on:click={onFindUser} loading={isSearching} />
-			</div>
-
-			{#if twitchProfile && twitchProfile.id}
-				<div class="results">
-					<img src={twitchProfile.profile_image_url} />
-					<div>
-						<h1 class="title is-4">{twitchProfile.display_name}</h1>
-						<h2 class="subtitle is-6">
-							<a href="https://twitch.tv/{encodeURIComponent(twitchProfile.login)}" target="_blank" rel="noreferrer"
-								>https://twitch.tv/{twitchProfile.login}</a>
-						</h2>
-						<p>{twitchProfile.description}</p>
-					</div>
-				</div>
-			{:else if error}
-				<p class="error">{error}</p>
-			{:else if alreadySearched}
-				<p>Twitch user not found.</p>
-			{/if}
-		</svelte:fragment>
-
-		<svelte:fragment slot="footer-right">
-			<Button
-				iconFa="fab fa-twitch"
-				label="Link"
-				type="twitch"
-				on:click={() => dispatch('link', twitchProfile)}
-				disabled={!twitchProfile} />
-			<Button label="Cancel" on:click={onCancel} />
-		</svelte:fragment>
-	</Dialog>
-{/if}
-
-<style>
-	.header-title {
-		text-align: left;
-	}
-
-	.search {
-		display: flex;
-		justify-content: center;
-		align-items: center;
-		margin-bottom: 1em;
-	}
-
-	input {
-		width: 100%;
-		padding: calc(0.65em - 1px) 1em;
-		margin: 0 0 0.45em 0;
-		outline: none;
-		color: var(--textColor);
-		background-color: var(--faded);
-		border: 2px solid var(--faded);
-		border-top-right-radius: 0;
-		border-bottom-right-radius: 0;
-	}
-
-	.search :global(.button) {
-		border-top-left-radius: 0;
-		border-bottom-left-radius: 0;
-	}
-
-	.results {
-		display: flex;
-		justify-content: flex-start;
-		align-items: center;
-	}
-
-	img {
-		width: 30%;
-		height: auto;
-		margin-right: 1.5em;
-		border-radius: 50%;
-	}
-
-	a {
-		color: #9146ff !important;
-		word-wrap: break-word;
-	}
-
-	.error {
-		color: var(--error);
-	}
-</style>

src/pages/Player.svelte
@@ -3,7 +3,6 @@
 	import {navigate} from 'svelte-routing';
 	import {fade} from 'svelte/transition';
 	import createPlayerInfoWithScoresStore from '../stores/http/http-player-with-scores-store';
-	import createTwitchService from '../services/twitch';
 	// import createAccSaberService from '../services/accsaber';
 	import createPinnedScoresStore from '../stores/beatleader/pinned-scores';
 	import createStatsHistoryStore from '../stores/beatleader/stats-history';
@@ -19,7 +18,6 @@
 	import Scores from '../components/Player/Scores.svelte';
 	import MiniRankings from '../components/Ranking/MiniRankings.svelte';
 	import AccSaberMiniRanking from '../components/Ranking/AccSaberMini.svelte';
-	import TwitchVideos from '../components/Player/TwitchVideos.svelte';
 	import ContentBox from '../components/Common/ContentBox.svelte';
 	import CardsCarousel from '../components/Player/CardsCarousel.svelte';
 	import PinnedScores from '../components/Player/PinnedScores.svelte';
@@ -92,9 +90,6 @@
 		return {service, serviceParams};
 	}
 
-	const twitchService = createTwitchService();
-	let twitchVideos = [];
-
 	// const accSaberService = createAccSaberService();
 
 	function onPageChanged(event) {
@@ -134,13 +129,6 @@
 		}
 	}
 
-	async function updateTwitchProfile(playerId) {
-		if (!playerId) return;
-
-		const twitchProfile = await twitchService.refresh(playerId);
-		twitchVideos = twitchProfile && twitchProfile.videos && twitchProfile.videos.length ? twitchProfile.videos : [];
-	}
-
 	let avatarHash = '';
 	async function onPlayerDataUpdated() {
 		if (playerStore) {
@@ -150,18 +138,6 @@
 			avatarHash = (Math.random() * 100000).toString();
 		}
 	}
-
-	onMount(async () => {
-		const twitchUnsubscribe = eventBus.on('player-twitch-videos-updated', ({playerId: twitchPlayerId, twitchProfile}) => {
-			if (twitchPlayerId !== currentPlayerId) return;
-
-			twitchVideos = twitchProfile && twitchProfile.videos && twitchProfile.videos.length ? twitchProfile.videos : [];
-		});
-
-		return () => {
-			twitchUnsubscribe();
-		};
-	});
 
 	let innerWidth = 0;
 	let innerHeight = 0;
@@ -183,7 +159,6 @@
 		?.split('/')
 		.map(s => capitalize(s))
 		.join(' / ')} - ${ssrConfig.name}`;
-	$: updateTwitchProfile(currentPlayerId);
 
 	$: playerData = $playerStore;
 	$: playerId = playerData?.playerId ?? null;
@@ -235,7 +210,6 @@
 				isLoading={$playerIsLoading}
 				error={$playerError}
 				{skeleton}
-				{twitchVideos}
 				on:player-data-updated={onPlayerDataUpdated}
 				{avatarHash}
 				fixedBrowserTitle={browserTitle}
@@ -243,7 +217,7 @@
 
 			{#if !$editModel}
 				{#if $configStore.profileParts.graphs}
-					<CardsCarousel {playerId} {playerInfo} {scoresStats} {ssBadges} {twitchVideos} {playerData} />
+					<CardsCarousel {playerId} {playerInfo} {scoresStats} {ssBadges} {playerData} />
 				{/if}
 				{#if $configStore.profileParts.pinnedScores}
 					<PinnedScores {pinnedScoresStore} {playerId} fixedBrowserTitle={browserTitle} />
@@ -275,12 +249,6 @@
 	{#if innerWidth > 1749 && ($configStore.profileParts.globalMiniRanking || $configStore.profileParts.countryMiniRanking || $configStore.profileParts.friendsMiniRanking)}
 		<aside>
 			<MiniRankings {rank} {country} {countryRank} box={true} />
-
-			{#if twitchVideos && twitchVideos.length}
-				<ContentBox>
-					<TwitchVideos videos={twitchVideos} />
-				</ContentBox>
-			{/if}
 
 			{#if playerInfo?.clans?.filter(cl => cl.tag == 'FELA').length}
 				<ContentBox>

src/pages/Twitch.svelte
@@ -1,71 +1,0 @@
-<script>
-	import {onMount} from 'svelte';
-	import {navigate} from 'svelte-routing';
-	import {fade} from 'svelte/transition';
-	import createTwitchService from '../services/twitch';
-	import ssrConfig from '../ssr-config';
-	import {SsrHttpUnauthenticatedError} from '../network/errors';
-	import Error from '../components/Common/Error.svelte';
-	import Spinner from '../components/Common/Spinner.svelte';
-	import ContentBox from '../components/Common/ContentBox.svelte';
-
-	document.body.classList.add('slim');
-	document.body.scrollIntoView({behavior: 'smooth'});
-
-	let twitchService = createTwitchService();
-
-	let message = null;
-	let error = null;
-
-	onMount(async () => {
-		try {
-			const token = twitchService.getTwitchTokenFromUrl();
-
-			message = 'Token validation, please wait...';
-
-			await twitchService.processToken(token.accessToken);
-
-			message = 'Token has been validated correctly. Redirecting...';
-
-			navigate(token.url && token.url.length ? token.url : '/');
-		} catch (err) {
-			if (err instanceof SsrHttpUnauthenticatedError) {
-				try {
-					const jsonResponse = await err.response.json();
-
-					if (jsonResponse && jsonResponse.message) err = jsonResponse.message;
-				} catch (foo) {
-					// swallow error
-				}
-			}
-
-			error = err;
-		}
-	});
-</script>
-
-<svelte:head>
-	<title>Twitch integration | {ssrConfig.name}</title>
-</svelte:head>
-
-<article transition:fade|global>
-	<ContentBox>
-		<h1 class="title is-3">Twitch integration</h1>
-
-		{#if !error}
-			<Spinner /> {message}
-		{:else}
-			<p>Something went wrong. Twitch returned error.</p>
-
-			<Error {error} />
-		{/if}
-
-		<p><a href="" on:click|preventDefault={() => navigate('/')}>Back to Home</a></p>
-	</ContentBox>
-</article>
-
-<style>
-	article {
-		text-align: center;
-	}
-</style>

src/services/twitch.js
@@ -1,196 +1,0 @@
-import queues from '../network/queues/queues';
-import keyValueRepository from '../db/repository/key-value';
-import twitchRepository from '../db/repository/twitch';
-import createPlayerService from './beatleader/player';
-import profileApiClient from '../network/clients/twitch/api-profile';
-import videosApiClient from '../network/clients/twitch/api-videos';
-import eventBus from '../utils/broadcast-channel-pubsub';
-import log from '../utils/logger';
-import {addToDate, dateFromString, durationToMillis, formatDate, millisToDuration, MINUTE} from '../utils/date';
-import {PRIORITY} from '../network/queues/http-queue';
-import makePendingPromisePool from '../utils/pending-promises';
-
-const TWITCH_TOKEN_KEY = 'twitchToken';
-
-const REFRESH_INTERVAL = 5 * MINUTE;
-
-let service = null;
-let serviceCreationCount = 0;
-export default () => {
-	serviceCreationCount++;
-	if (service) return service;
-
-	const resolvePromiseOrWaitForPending = makePendingPromisePool();
-
-	const playerService = createPlayerService();
-
-	const getAuthUrl = (state = '', scopes = '') => queues.TWITCH.getAuthUrl(state, scopes);
-
-	const getTwitchTokenFromUrl = () => {
-		const url = new URL(document.location);
-
-		const error = url.searchParams.get('error');
-		if (error) {
-			const errorMsg = url.searchParams.get('error_description');
-			throw new Error(errorMsg ? errorMsg : error);
-		}
-
-		const hash = url.hash;
-		if (!hash || !hash.length) throw new Error('Twitch did not return access token');
-
-		const accessTokenMatch = /access_token=(.*?)(&|$)/.exec(hash);
-		if (!accessTokenMatch) throw new Error('Twitch did not return access token');
-
-		const stateMatch = /state=(.*?)(&|$)/.exec(hash);
-
-		return {accessToken: accessTokenMatch[1], url: stateMatch ? decodeURIComponent(stateMatch[1]) : ''};
-	};
-
-	const processToken = async accessToken => {
-		// validate token
-		const tokenValidation = (await queues.TWITCH.validateToken(accessToken)).body;
-
-		const expiresIn = tokenValidation.expires_in * 1000;
-
-		const twitchToken = {
-			...tokenValidation,
-			accessToken,
-			obtained: new Date(),
-			expires: new Date(Date.now() + expiresIn),
-			expires_in: expiresIn,
-		};
-
-		await keyValueRepository().set(twitchToken, TWITCH_TOKEN_KEY);
-
-		eventBus.publish('twitch-token-refreshed', twitchToken);
-
-		return twitchToken;
-	};
-
-	const getCurrentToken = async () => keyValueRepository().get(TWITCH_TOKEN_KEY, true);
-
-	const fetchProfile = async (login, priority = PRIORITY.FG_LOW, {fullResponse = false, ...options} = {}) => {
-		const token = await getCurrentToken();
-		if (!token || !token.expires || token.expires <= new Date()) return null;
-
-		return resolvePromiseOrWaitForPending(`profileApiClient/${login}/${fullResponse}`, () =>
-			profileApiClient.getProcessed({...options, accessToken: token.accessToken, login, priority, fullResponse})
-		);
-	};
-
-	const fetchVideos = async (userId, priority = PRIORITY.FG_LOW, {fullResponse = false, ...options} = {}) => {
-		const token = await getCurrentToken();
-		if (!token || !token.expires || token.expires <= new Date()) return null;
-
-		return resolvePromiseOrWaitForPending(`videosApiClient/${userId}/${fullResponse}`, () =>
-			videosApiClient.getProcessed({...options, accessToken: token.accessToken, userId, priority, fullResponse})
-		);
-	};
-
-	const getPlayerProfile = async playerId => twitchRepository().get(playerId);
-	const updatePlayerProfile = async twitchProfile => twitchRepository().set(twitchProfile);
-
-	const refresh = async (playerId, forceUpdate = false, priority = queues.PRIORITY.FG_LOW, throwErrors = false) => {
-		log.trace(`Starting Twitch videos refreshing${forceUpdate ? ' (forced)' : ''}...`, 'TwitchService');
-
-		if (!playerId) {
-			log.debug(`No playerId provided, skipping`, 'TwitchService');
-
-			return null;
-		}
-
-		try {
-			let twitchProfile = await twitchRepository().get(playerId);
-			if (!twitchProfile || !twitchProfile.login) {
-				log.debug(`Twitch profile for player ${playerId} is not set, skipping`, 'TwitchService');
-
-				return null;
-			}
-
-			const lastUpdated = twitchProfile.lastUpdated;
-			if (!forceUpdate) {
-				if (lastUpdated && lastUpdated > new Date() - REFRESH_INTERVAL) {
-					log.debug(
-						`Refresh interval not yet expired, skipping. Next refresh on ${formatDate(addToDate(REFRESH_INTERVAL, lastUpdated))}`,
-						'TwitchService'
-					);
-
-					return twitchProfile;
-				}
-			}
-
-			if (!twitchProfile.id) {
-				const fetchedProfile = await fetchProfile(twitchProfile.login);
-				if (!fetchedProfile) {
-					log.debug(`Can not fetch Twitch profile for player ${playerId}, skipping`, 'TwitchService');
-
-					return twitchProfile;
-				}
-
-				twitchProfile = {...twitchProfile, ...fetchedProfile, playerId};
-
-				await updatePlayerProfile(twitchProfile);
-			}
-
-			const videos = await fetchVideos(twitchProfile.id);
-
-			twitchProfile.videos = videos;
-			twitchProfile.lastUpdated = new Date();
-			await updatePlayerProfile(twitchProfile);
-
-			if (videos && videos.length) {
-				eventBus.publish('player-twitch-videos-updated', {
-					playerId,
-					twitchProfile,
-				});
-			}
-
-			return twitchProfile;
-		} catch (e) {
-			if (throwErrors) throw e;
-
-			log.debug(`Twitch player ${playerId} refreshing error`, 'TwitchService', e);
-
-			return null;
-		}
-	};
-
-	async function findTwitchVideo(playerTwitchProfile, timeset, songLength) {
-		if (!playerTwitchProfile || !playerTwitchProfile.videos || !timeset || !songLength) return null;
-
-		const songStarted = addToDate(-songLength * 1000, timeset);
-		const video = playerTwitchProfile.videos
-			.map(v => ({
-				...v,
-				created_at: dateFromString(v.created_at),
-				ended_at: addToDate(durationToMillis(v.duration), dateFromString(v.created_at)),
-			}))
-			.find(v => v.created_at <= songStarted && songStarted < v.ended_at);
-
-		return video ? {...video, url: video.url + '?t=' + millisToDuration(songStarted - video.created_at)} : null;
-	}
-
-	const destroyService = () => {
-		serviceCreationCount--;
-
-		if (serviceCreationCount === 0) {
-			service = null;
-			playerService.destroyService();
-		}
-	};
-
-	service = {
-		getAuthUrl,
-		getTwitchTokenFromUrl,
-		processToken,
-		getCurrentToken,
-		getPlayerProfile,
-		updatePlayerProfile,
-		fetchProfile,
-		findTwitchVideo,
-		refresh,
-		destroyService,
-	};
-
-	return service;
-};

src/stores/http/enhancers/scores/twitch.js
@@ -1,27 +1,0 @@
-import createTwitchService from '../../../../services/twitch';
-import {findDiffInfoWithDiffAndTypeFromBeatMaps} from '../../../../utils/beatleader/song';
-import {opt} from '../../../../utils/js';
-
-let twitchService;
-
-export default async (data, playerId = null) => {
-	if (!data || !data.score || !data.leaderboard || !data.leaderboard.beatMaps) return;
-
-	const versions = opt(data, 'leaderboard.beatMaps.versions');
-	const versionsLastIdx = versions && Array.isArray(versions) && versions.length ? versions.length - 1 : 0;
-	const bmStats = findDiffInfoWithDiffAndTypeFromBeatMaps(
-		opt(data, `leaderboard.beatMaps.versions.${versionsLastIdx}.diffs`),
-		data.leaderboard.diffInfo
-	);
-	if (!bmStats || !bmStats.seconds) return;
-
-	if (!twitchService) twitchService = createTwitchService();
-
-	const twitchProfile = await twitchService.refresh(playerId);
-	if (!twitchProfile) return;
-
-	const video = await twitchService.findTwitchVideo(twitchProfile, data.score.timeSet, bmStats.seconds);
-	if (!video || !video.url) return;
-
-	data.twitchVideo = video;
-};

commit 5154d7c3d98941a82d33de8f7d8d83dad3bfdc13

fix: immer error introduced in ec36211c

src/stores/http/http-scores-store.js
@@ -87,15 +87,13 @@ export default (
 
 		for (const scoreRow of newState) {
 			if (currentService !== 'accsaber') {
-				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => ppAttributionEnhancer(draft, currentPlayerId))
-					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow))
-					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow)))
-					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow));
+				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => ppAttributionEnhancer(draft, currentPlayerId)).then(
+					scoreRow => setStateRow(enhanceTaskId, scoreRow)
+				);
 			} else {
-				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => beatMapsEnhancer(draft))
-					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow))
-					.then(scoreRow => stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow)))
-					.then(scoreRow => setStateRow(enhanceTaskId, scoreRow));
+				stateProduce(scoreRow, getPatchId(currentPlayerId, scoreRow), draft => beatMapsEnhancer(draft)).then(scoreRow =>
+					setStateRow(enhanceTaskId, scoreRow)
+				);
 			}
 		}
 	};