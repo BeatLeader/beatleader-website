d66ddd4aab4ccf82e62dc74d9887b1652bb7d95e
LightAi39 <66924896+LightAi39@users.noreply.github.com>
2024-02-24 01:56:19 +0100

    actually disable banner - sorry xD

--- src/pages/Ranking.svelte
+++ src/pages/Ranking.svelte
@@ -367,16 +367,16 @@
 
 <section class="align-content">
 	<article class="page-content" transition:fade|global>
-		<ContentBox cls="event-banner" on:click={() => navigate('/event/44')}>
+		<!-- <ContentBox cls="event-banner" on:click={() => navigate('/event/44')}>
 			<div class="event-container">
-				<!-- <img alt="Event banner" class="event-image" src="https://cdn.assets.beatleader.xyz/71875-event-s.png" /> -->
-				<!-- <div class="event-text-container">
+				<img alt="Event banner" class="event-image" src="https://cdn.assets.beatleader.xyz/71875-event-s.png" />
+				<div class="event-text-container">
 					<span class="event-title">Ranked early 2024!</span>
 					<span class="event-text">Check out what was ranked and compete for a badge.</span>
-				</div> -->
+				</div>
 				<Button label="Event" iconFa="fas fa-rocket" on:click={() => navigate('/event/44')} />
 			</div>
-		</ContentBox>
+		</ContentBox> -->
 
 		<ContentBox bind:box={boxEl}>
 			<h1 class="title is-5">
