f20a238192d47f2a4485d8820c9a3c2002107779
NSGolova <golova@golova.dev>
2024-08-01 21:58:08 +0100

    Added analyzer.net to keep the auth

--- src/network/queues/beatleader/api-queue.js
+++ src/network/queues/beatleader/api-queue.js
@@ -26,7 +26,15 @@ export const BL_REPLAYS_URL = (() => {
 		return 'https://replay.beatleader.xyz/';
 	}
 })();
-export const BL_ANALYZER_URL = 'https://analyzer.beatleader.xyz/';
+export const BL_ANALYZER_URL = (() => {
+	if (location.host.includes('stage')) {
+		return 'https://stage.analyzer.beatleader.net/';
+	} else if (location.host.includes('beatleader.net')) {
+		return 'https://analyzer.beatleader.net/';
+	} else {
+		return 'https://analyzer.beatleader.xyz/';
+	}
+})();
 export const BL_RENDERER_API_URL = 'https://render.beatleader.xyz/';
 export const BL_SOCKET_URL = 'wss://sockets.api.beatleader.xyz/';
 export const STEAM_API_URL = '/cors/steamapi';
